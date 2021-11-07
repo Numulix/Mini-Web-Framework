4c114bfedce4d7e608f92aa39dbe53158fa3dcde
Jovan Babic <jbabic999@gmail.com>
2021-11-07 18:34:49 +0100

    Qualifiers

--- src/main/java/engine/Engine.java
+++ src/main/java/engine/Engine.java
@@ -1,16 +1,16 @@
 package engine;
 
 
-import annotations.Bean;
-import annotations.Controller;
-import annotations.Service;
+import annotations.*;
 import framework.MiniWebFramework;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // Predstavlja Dependency Injection Engine (DI Engine)
 public class Engine {
@@ -54,13 +54,14 @@ public class Engine {
         return classList;
     }
 
-    public static void initAllClasses() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void initAllClasses() throws Exception {
         List<Class> allClasses = getClassesFromPackage("");
         for (Class cl : allClasses) {
             if (cl.isAnnotationPresent(Controller.class)) {
                 System.out.println("!- Engine -! Klasa " + cl.getName() + " je kontroler");
                 MiniWebFramework.addControllerClass(cl);
                 controllerClassMap.put(cl.getName(), cl.getConstructor().newInstance());
+                classMap.put(cl.getName(), cl.getConstructor().newInstance());
             }
 
             if ((cl.isAnnotationPresent(Bean.class) && (((Bean) cl.getAnnotation(Bean.class)).scope().equals("singleton")))
@@ -68,7 +69,38 @@ public class Engine {
                 classMap.put(cl.getName(), cl.getConstructor().newInstance());
                 System.out.println("!- Engine -! Napravljena nova instanca klase " + cl.getName());
             }
+
+            if (cl.isAnnotationPresent(Qualifier.class)) {
+
+                if (checkIfBeanServiceComponent(cl)) {
+                    Qualifier q = (Qualifier) cl.getDeclaredAnnotation(Qualifier.class);
+                    String value = q.value();
+
+                    if (dc.getQualifiers().get(value) != null) {
+                        try {
+                            throw new Exception("Class already has qualifier");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    dc.getQualifiers().put(value, cl);
+                }
+            }
         }
+
+        for (Map.Entry<String, Object> entry: classMap.entrySet()) {
+            injectObjectAnnotations(entry.getValue());
+        }
+    }
+
+    private static boolean checkIfBeanServiceComponent(Class cl) {
+        return cl.isAnnotationPresent(Bean.class) || cl.isAnnotationPresent(Service.class) || cl.isAnnotationPresent(Component.class);
+    }
+
+    public static void injectObjectAnnotations(Object obj) {
+        Class cl = obj.getClass();
+        Field[] fields = cl.getDeclaredFields();
     }
 
 }
